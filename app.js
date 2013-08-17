4978434f1c4f3a37a395eb68670b21e6a4814a64
Clark Gredoña <cgredona@gmail.com>
2013-08-17 17:49:48 -0400

    Added basic S3 upload functionality

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.get('/', routes.index);
 app.get('/:name', routes.school);
 app.get('/api/school/:name', routes.getSchoolJSON);  // human readable
 app.get('/api/class/:class-id', routes.getClassJSON);  // class id
-app.get('/api/upload/', routes.upload);
+app.get('/api/upload/', routes.uploadForm);
+app.post('/api/upload/', routes.upload);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
