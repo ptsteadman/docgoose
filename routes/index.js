ebb60090c253baa561237903827805ec9c8f8c3e
Patrick <uhhonestly@gmail.com>
2013-08-17 15:51:54 -0400

    adding GeoIP lookup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
+var motherGoose = require('./mothergoose');
+var geoip = require('geoip-lite');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	//get IP address and send to correct school
+	var location = geoip.lookup(req.connection.remoteAddress);
+	console.log(location);
+  res.redirect('/cornell');
 };
 
 exports.school = function(req, res){
 	var school = req.params.name;
+	console.log(school);
 	res.render('index', {title: school})
 }
 
