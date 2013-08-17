54b9dbad0c1a59500a0623fc86865cd50d5f79b1
Patrick <uhhonestly@gmail.com>
2013-08-17 16:09:14 -0400

    Geoip fixed

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var geoip = require('geoip-lite');
 
 exports.index = function(req, res){
 	//get IP address and send to correct school
-	console.log(req.connection.remoteAddress);
-	var location = geoip.lookup(req.connection.remoteAddress);
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	console.log(ip);
+	var location = geoip.lookup(ip);
 	console.log(location);	
   res.redirect('/cornell');
 };
