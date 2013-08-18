815c6820a451c49855b41e7685cc27e4e02d90fb
Clark Gredoña <cgredona@gmail.com>
2013-08-18 04:17:34 -0400

    Refactored code, migrating the localization in index.js to its own file distanceSorter.js

---
+++ util/distanceSorter.js
@@ -0,0 +1,29 @@
+module.exports = {
+    sortSchoolsByDistanceFrom: function(schools, latitude, longitude) {
+        console.log("sorting schools");
+
+        var length = schools.length;
+        for (var i = 0; i < length; i++) {
+            console.log(schools[i].name + " : ");
+            module.exports.computeDistanceFromSchool(schools[i], latitude, longitude);
+        }
+
+        schools.sort(function (a, b) {
+            if (a.distance > b.distance) {
+                return 1;
+            }
+            return -1;
+        });
+    },
+
+    computeDistanceFromSchool: function(school, latitude, longitude) {
+        school.distance = module.exports.computeDistanceBetween(school.latitude, school.longitude, latitude, longitude);
+    },
+
+    computeDistanceBetween: function(latitude1, longitude1, latitude2, longitude2) {
+        console.log("Computing the distance between (" + latitude1 + ", " + longitude1 + ") and (" + latitude2 + ", " + longitude2 + ").");
+        var xDistance = latitude1 - latitude2;
+        var yDistance = longitude1 - longitude2;
+        return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+    }
+}
