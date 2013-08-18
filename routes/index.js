99b18910025d12368052e2d84b8dc9f632e2ddc0
Clark Gredoña <cgredona@gmail.com>
2013-08-18 03:45:19 -0400

    Added redirecting to appropriate based on geo-ip address! Also added some Canadian schools;

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,47 @@ var AWS = require('aws-sdk');
 var format = require('util').format;
 var fs = require('fs');
 var AWS = require('aws-sdk');
+var schools = [{"name":"cornell","latitude":42.4479,"longitude":-76.4779},{"name":"mcgill","latitude":45.5085,"longitude":-73.5797,"country":"canada"},{"name":"berkeley","latitude":37.8717,"longitude":-122.2610},{"name":"georgia tech","latitude": 33.7756,"longitude":-84.3963},{"name":"harvard","latitude":42.3762,"longitude":-71.1158},{"name":"illinois","latitude":40.0401,"longitude":-88.2707},{"name":"michigan","latitude":42.3184,"longitude":-83.2300},{"name":"mit","latitude":42.3584,"longitude":-71.0912},{"name":"nyu","latitude":40.7296,"longitude":-73.9952},{"name":"ohio state","latitude":40.7976,"longitude":-82.5818},{"name":"penn state","latitude":40.7982,"longitude":-77.8599},{"name":"purdue","latitude":40.4246,"longitude":-86.9107},{"name":"rutgers","latitude":40.5010,"longitude":-74.4473},{"name":"stanford","latitude":37.4278,"longitude":-122.1697},{"name":"texas","latitude":30.2879,"longitude":-97.7278},{"name":"texas a&m","latitude":30.6090,"longitude":-96.3492},{"name":"toronto","latitude":43.6602,"longitude":-79.3943},{"name":"usc","latitude":34.0223,"longitude":-118.2851},{"name":"virgina_tech","latitude":38.8967,"longitude":-77.1894},{"name":"washington","latitude":47.6556,"longitude":-122.3091},{"name":"yale","latitude":41.3104,"longitude":-72.9290}];
 
 exports.index = function (req, res) {
-
     //get IP address and send to correct school
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(ip);
     var location = geoip.lookup(ip);
-    console.log(location);
-    res.redirect('/cornell');
+	if (location) {
+		sortSchoolsByDistanceFrom(schools,location.ll[0],location.ll[1]);
+		console.log(location);	
+		res.redirect('/' + schools[0].name);
+	} else {
+		res.redirect('/cornell'); //Cornell
+	}	
 };
+
+function sortSchoolsByDistanceFrom(schools, latitude, longitude) {
+	console.log("sorting schools");
+	
+	var length = schools.length;
+	for (var i = 0; i < length; i++) {
+		console.log(schools[i].name + " : ");computeDistanceFromSchool(schools[i], latitude, longitude);
+	}
+	
+	schools.sort(function(a, b) {
+			if (a.distance > b.distance) {return 1;}
+			return -1;	
+	});
+}
+
+
+function computeDistanceFromSchool(school, latitude, longitude) {
+	school.distance = computeDistanceBetween(school.latitude, school.longitude, latitude, longitude);
+}
+
+function computeDistanceBetween(latitude1, longitude1, latitude2, longitude2) {
+	console.log("Computing the distance between (" + latitude1 + ", " +longitude1 + ") and (" + latitude2 + ", " + longitude2 + ").");
+	var xDistance = latitude1 - latitude2;
+	var yDistance = longitude1 - longitude2;
+	return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+}
 
 exports.school = function (req, res) {
     var school = req.params.name;
